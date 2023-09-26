9606b89286c2d0a80241b649eef69382fd6ea87d
cypresspy <111775950+cypresspy@users.noreply.github.com>
2023-09-26 15:28:45 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='adelin',
+    version='0.1.0',
+    packages=['adelin'],
+    url='https://github.com/cypresspy/adelin',
+    license='MIT',
+    author='M.E.K',
+    author_email='info@adelin.com.tr',
+    install_requires=[],
+    description='Make simple database, store and manage'
+)
